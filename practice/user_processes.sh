144dd45347be855638c0b869c6e66f080feee18e
Vlad <vladyslav.samoilenko@proton.me>
2022-12-22 12:48:21 +0000

    feat: user processes script. Sorted/unsorted option

---
+++ practice/user_processes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+read -p "Would you like to sort the processes output by memory or CPU? (m/c): " sort_by
+read -p "How many procceses you would like it to display? Leave empty for all: " proc_count
+
+if [ "$proc_count" == "" ]; then
+	proc_count=100
+fi
+
+if [ "$sort_by" == "m" ]; then
+  ps aux --sort -rss | grep -i `whoami` | head -n "$proc_count"
+elif [ "$sort_by" == "c" ]; then
+  ps aux --sort -pcpu | grep -i `whoami` | head -n "$proc_count"
+else
+  echo -e "${RED}No input provided. Showing unsorted list${NC}"
+  ps aux | grep `whoami` | head -"$proc_count"
+fi
